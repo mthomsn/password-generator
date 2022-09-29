f48681b68110b1d4630f870a8f54b9653e78d937
Matt <matthewthomsen99@gmail.com>
2022-09-29 01:08:52 -0500

    changed varible name password because of conflict

--- script.js
+++ script.js
@@ -9,18 +9,21 @@ let all = [upperCase, lowerCase, numbers, specialCharacters];
 
 const chosen = {}; // object for storing user input 
 let working = []; // pools all symbols into one array to pull from
-let password = []; // final array where random password will land
+let passcode = []; // final array where random passcode will land
 let counter = 0; // when static variable is added to working array, i'm pushing one item from each array when adding to working array so this counter will count how many random characters have been added before finalizing random password
 
-// main function to create random password using user selected criteria
+// main function to create random passcode using user selected criteria
 function generatePassword() {
-  alert("You will be prompted with various choices for your password: \nPASSWORD LENGTH: Password can be " + minLength + " to " + maxLength + " characters long. \n\nUPPER CASE LETTERS: Decide if you want uppercase letters in your password. \n\nLOWER CASE LETTERS: Decide if you want lowercase letters in your password. \n\nNUMBERS: Decide if you want numbers in your password. \n\nSPECIAL CHARACTERS: Decide if you want special characters in your password.")
+  alert("You will be prompted with various choices for your password: \npasscode LENGTH: passcode can be " + minLength + " to " + maxLength + " characters long. \n\nUPPER CASE LETTERS: Decide if you want uppercase letters in your password. \n\nLOWER CASE LETTERS: Decide if you want lowercase letters in your password. \n\nNUMBERS: Decide if you want numbers in your password. \n\nSPECIAL CHARACTERS: Decide if you want special characters in your password.")
 
-  // user prompted with password criteria
+  // user prompted with passcode criteria
 
-  chosen.length = prompt("How long do you want your password to be? Choose a number between " + minLength + " and " + maxLength);
+  chosen.length = prompt("How long do you want your passcode to be? Choose a number between " + minLength + " and " + maxLength);
   // chosen.length = parseInt(chosen.length);
 
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
   // need to validate that user input is a number and an integer not a float
   while(chosen.length < minLength || chosen.length > maxLength || chosen.length == undefined) { 
     chosen.length = prompt("Please choose valid number between " + minLength + " and " + maxLength);
@@ -41,54 +44,55 @@ function generatePassword() {
   }
   }
  
-  // adding user character choices to working array, and adding one random character from each chosen character type
+  // adding user character choices to working array, and adding one random character from each chosen character type to password
   if (chosen.upperCase){
     working = working.concat(upperCase); // creating working array to pull random characters from
-    password.push(upperCase[Math.floor(Math.random() * upperCase.length)]); // ensuring one item from each selected array is included in final password
+    passcode.push(upperCase[Math.floor(Math.random() * upperCase.length)]); // ensuring one item from each selected array is included in final password
     counter++; // will subtract counter # from final array length because of the push above
   }
   if (chosen.lowerCase){
     working = working.concat(lowerCase);
-    password.push(lowerCase[Math.floor(Math.random() * lowerCase.length)]);
+    passcode.push(lowerCase[Math.floor(Math.random() * lowerCase.length)]);
     counter++;
   }
   if (chosen.numbers){
     working = working.concat(numbers);
-    password.push(numbers[Math.floor(Math.random() * numbers.length)]);
+    passcode.push(numbers[Math.floor(Math.random() * numbers.length)]);
     counter++;
   }
   if (chosen.specialChar){
     working = working.concat(specialCharacters);
-    password.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
+    passcode.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
     counter++;
   }
 
   // assembling password
   for(var i = 0; i < chosen.length - counter; i++) {
-    password.push(working[Math.floor(Math.random() * working.length)]);
+    passcode.push(working[Math.floor(Math.random() * working.length)]);
   }
-  console.log(password);
-  // randomizing password character order
-  shuffleArray(password);
-  console.log(password);
+  console.log(passcode);
+  // randomizing passcode character order
+  shuffleArray(passcode);
+  console.log(passcode);
   // remove spaces
-  password = password.join("").trim();
-  console.log(password);
+  passcode = passcode.join("").trim();
+  console.log(passcode);
+  return passcode;
 
-  // password is either displayed as alert or written to the page
+  // passcode is either displayed as alert or written to the page
   
 }
 
-function shuffleArray(array) {
+function shuffleArray(array) { 
   return array.sort(()=>Math.random()-0.5);
 }
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password= generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
